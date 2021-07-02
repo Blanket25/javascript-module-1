06988de10c2fd36d95ab75e8283f33c7708f1a38
Blanket25 <bianca_inga@hotmail.com>
2021-07-02 09:13:31 +0200

    a correction on first exercise

--- week-1/Homework/F-strings-methods/exercise2.js
+++ week-1/Homework/F-strings-methods/exercise2.js
@@ -1,5 +1,5 @@
-const name = " Daniel  ";
-const nameNoSpaces = name.trim();
+const firstName = " Daniel  ";
+const nameNoSpaces = firstName.trim();
 
 const message = "My name is " + nameNoSpaces + " and my name is " + nameNoSpaces.length + " characters long";
 
